722aded0cae181a70b689b71118576bd2460b539
Hisham <hishamshefeekh@gmail.com>
2025-10-27 03:05:45 +0530

    sierpinski

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import argparse
+import itertools
+import typing
 from turtle import Turtle
-from typing import cast
+
+PADDING = 10
 
 Point = tuple[float, float]
 Triangle = tuple[Point, Point, Point]
@@ -15,24 +18,46 @@ class SierpinskiEnvelopeTurtle(Turtle):
 
         self.pencolor("white")
 
-    def _goto_without_drawing(self, x: float, y: float) -> None:
+    def _goto_without_drawing(self, point: Point) -> None:
         self.penup()
-        self.goto(x, y)
+        self.goto(point)
         self.pendown()
 
     def _draw_equilateral_triangle(self, size: float) -> Triangle:
         self.begin_poly()
 
-        for _ in range(3):
+        for i in range(3):
+            if i == 2:
+                self.end_poly()  # this is to prevent a 4th point (1st one)
+
             self.forward(size)
             self.left(120)
 
-        self.end_poly()
+        return typing.cast(Triangle, self.get_poly())
 
-        return cast(Triangle, self.get_poly())
+    def _sierpinski(self, triangle: Triangle, depth: int) -> None:
+        if depth == 0:
+            return
 
-    def draw_pattern(self, depth) -> None:
-        triangle_side_length = self.screen.window_height() / 2 - 50
+        midpoints = []
+        for point_pair in zip(triangle, itertools.chain(triangle[1:], triangle[:1])):
+            midpoint = (
+                (point_pair[0][0] + point_pair[1][0]) / 2,
+                (point_pair[0][1] + point_pair[1][1]) / 2,
+            )
+
+            midpoints.append(midpoint)
+
+        self._goto_without_drawing(midpoints[2])
+        for midpoint in midpoints:
+            self.goto(midpoint)
+
+        for i in range(3):
+            new_triangle = (midpoints[i], midpoints[(i + 1) % 3], triangle[(i + 1) % 3])
+            self._sierpinski(new_triangle, depth - 1)
+
+    def draw_pattern(self, depth: int) -> None:
+        triangle_side_length = self.screen.window_height() / 2 - PADDING
 
         triangles = []
         for i in range(6):
@@ -41,6 +66,9 @@ class SierpinskiEnvelopeTurtle(Turtle):
                 triangles.append(triangle)
 
             self.left(60)
+
+        for triangle in triangles:
+            self._sierpinski(triangle, depth)
 
         self.screen.mainloop()
 
@@ -60,7 +88,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--depth",
         type=unsigned_int,
-        default=10,
+        default=6,
         help="Recursion depth of the pattern",
     )
 
